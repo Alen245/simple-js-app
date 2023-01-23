ae862471bc6e5ccb0e1ac5b98c65ec75c93257df
Alen245 <akiyanovski24@gmail.com>
2023-01-23 13:55:10 -0500

    for loop try again

--- scripts.js
+++ scripts.js
@@ -1,7 +1,7 @@
 let pokemonList = [{name:"Alakazam", height:1.5,type:"Psychic"},{name:"Meowth", height:0.4 , type:"Normal"},{name:"Onix",height:8.8 , type:["Rock","Earth"]}]
 
-let i = 0;
-for (;pokemonList[i];){
+
+for (let i = 0; i < pokemonList.length; i++){
     text = " " + pokemonList[i];
     i++
 }
