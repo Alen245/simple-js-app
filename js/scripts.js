37b746fbd6b0cdbe52e7775d867ec8ab9c795ea6
Alen245 <akiyanovski24@gmail.com>
2023-01-23 13:58:10 -0500

    replaced code

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,7 @@
-alert('Hello world');
-let favoriteFood = "Shawarma";
-document.write(favoriteFood);
+let pokemonList = [{name:"Alakazam", height:1.5,type:"Psychic"},{name:"Meowth", height:0.4 , type:"Normal"},{name:"Onix",height:8.8 , type:["Rock","Earth"]}]
+
+
+for (let i = 0; i < pokemonList.height; i++){
+    text = " " + pokemonList[i];
+    i++
+}
