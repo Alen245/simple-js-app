bd44664613ff9064bcdd3d93141c818e87a35707
Alen245 <akiyanovski24@gmail.com>
2023-01-30 14:21:24 -0500

    loadlist

--- js/scripts.js
+++ js/scripts.js
@@ -29,6 +29,36 @@ function addListItem(pokemon){
 
 }
 
+function loadList() {
+  return fetch(apiUrl).then(function (response) {
+    return response.json();
+  }).then(function (json) {
+    json.results.forEach(function (item) {
+      let pokemon = {
+        name: item.name,
+        detailsUrl: item.url
+      };
+      add(pokemon);
+    });
+  }).catch(function (e) {
+    console.error(e);
+  })
+}
+
+return {
+  add: add,
+  getAll: getAll,
+  loadList: loadList
+};
+})();
+
+pokemonRepository.loadList().then(function() {
+// Now the data is loaded!
+pokemonRepository.getAll().forEach(function(pokemon){
+  pokemonRepository.addListItem(pokemon);
+});
+});
+
 
 
 return {
