fef31004157100247552f0c5e4a894ddd1b07e93
Alen245 <akiyanovski24@gmail.com>
2023-01-30 14:07:59 -0500

    load list

--- js/scripts.js
+++ js/scripts.js
@@ -15,6 +15,43 @@ const pokemonList = [
     height:8.8 , 
     type:["Rock","Earth"]
     } ]
+
+    let pokemonRepository = (function () {
+      let pokemonList = [];
+      let apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=150';
+    
+      // Other functions remain here
+    
+      function loadList() {
+        return fetch(apiUrl).then(function (response) {
+          return response.json();
+        }).then(function (json) {
+          json.results.forEach(function (item) {
+            let pokemon = {
+              name: item.name,
+              detailsUrl: item.url
+            };
+            add(pokemon);
+          });
+        }).catch(function (e) {
+          console.error(e);
+        })
+      }
+    
+      return {
+        add: add,
+        getAll: getAll,
+        loadList: loadList
+      };
+    })();
+    
+    pokemonRepository.loadList().then(function() {
+      // Now the data is loaded!
+      pokemonRepository.getAll().forEach(function(pokemon){
+        pokemonRepository.addListItem(pokemon);
+      });
+    });
+
 function add(pokemon) {
   pokemonList.push(pokemon);
 }
