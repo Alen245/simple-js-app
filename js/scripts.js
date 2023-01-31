7e8d53d5f01f02dc4f80b5ee3341fcf3001efe04
Alen245 <akiyanovski24@gmail.com>
2023-01-31 17:09:08 -0500

    minor changes

--- js/scripts.js
+++ js/scripts.js
@@ -44,7 +44,7 @@ function loadList() {
         detailsUrl: item.url
       };
       add(pokemon);
-      console.log(pokemon);
+      
     });
   }).catch(function (e) {
     console.error(e);
@@ -56,7 +56,7 @@ function loadDetails(item) {
   return fetch(url).then(function (response) {
     return response.json();
   }).then(function (details) {
-    // Now we add the details to the item
+    // add the details to the item
     item.imageUrl = details.sprites.front_default;
     item.height = details.height;
     item.types = details.types;
